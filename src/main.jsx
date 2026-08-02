6e99edca3aa45d8e4db14310d228d2df941e47eb
Mo7amedTa7a <mt4658740@gmail.com>
2026-08-03 02:37:55 +0300

    handling response and request axios Instance

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { ThemeProvider  } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./Theme/Theme.jsx";
 
-
-
 createRoot(document.getElementById("root")).render(
   <ThemeProvider  theme={theme}>
       <CssBaseline />
--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,11 +1,53 @@
 import axios from "axios";
 
+let baseURL;
 
-
+if (import.meta.env.DEV) {
+  baseURL = import.meta.env.VITE_BASE_URL_DEV;
+  // console.log(import.meta.env);
+} else {
+  baseURL = import.meta.env.VITE_BASE_URL_PRO;
+}
 const axiosInstance = axios.create({
-    baseURL:"https://fakestoreapi.com/",
-    timeout:1000,
-    headers:{"X-Custom-Header":"Mhmed"}
-})
+  baseURL,
+  timeout: 5000,
+  headers: {
+    "X-Custom-Header": "Mhmed",
+  },
+});
 
-export default axiosInstance
+axiosInstance.interceptors.request.use(
+  function (config) {
+    const token = "userToken:34523mrenfcsdvccvsdf23rfewfdc";
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+  { runWhen: (config) => config.url !== "login" },
+);
+
+// Response
+axiosInstance.interceptors.response.use(
+  (response) => response,
+
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+     if (error.response?.status === 403) {
+      console.log("You don't have permission");
+    }
+
+    if (error.response?.status === 404) {
+      console.log("Resource not found");
+    }
+
+    if (error.response?.status >= 500) {
+      console.log("Server error");
+    }
+  },
+);
+export default axiosInstance;
